e88dedd3e2b7d7a4d616d355152ff02922686716
Anarhyroch <Anarhyroch@gmail.com>
2021-07-06 10:21:20 +0800

    add logger

--- src/main/java/org/csu/delivery/controller/userController.java
+++ src/main/java/org/csu/delivery/controller/userController.java
@@ -2,14 +2,17 @@ package org.csu.delivery.controller;
 
 import org.csu.delivery.entity.User;
 import org.csu.delivery.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class userController {
-
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     UserRepository userRepository;
 
@@ -21,6 +24,7 @@ public class userController {
 
     @GetMapping("/user")
     public User insertUser(User user) {
+        logger.info("save a user");
         User save = userRepository.save(user);
         return save;
     }
--- src/test/java/org/csu/delivery/CsuFoodDeliveryApplicationTests.java
+++ src/test/java/org/csu/delivery/CsuFoodDeliveryApplicationTests.java
@@ -1,7 +1,10 @@
 package org.csu.delivery;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
 
 @SpringBootTest
 class CsuFoodDeliveryApplicationTests {
